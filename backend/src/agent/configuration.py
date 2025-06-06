9e7d155ac7f910419a717cfe7e3e424fb60a04b7
YuyangXue_Ed <yuyang.xue@ed.ac.uk>
2025-06-06 10:46:24 +0000

    add changes to AzureChatOpenAI

--- backend/src/agent/configuration.py
+++ backend/src/agent/configuration.py
@@ -9,21 +9,28 @@ class Configuration(BaseModel):
     """The configuration for the agent."""
 
     query_generator_model: str = Field(
-        default="qwen2.5-coder-7b-instruct",
+        default="gpt-4.1",
         metadata={
             "description": "The name of the language model to use for the agent's query generation."
         },
     )
 
+    reasoning_model: str = Field(
+        default="o3",
+        metadata={
+            "description": "The name of the language model to use for the agent's reasoning."
+        },
+    )
+
     reflection_model: str = Field(
-        default="qwen2.5-coder-7b-instruct",
+        default="gpt-4.1",
         metadata={
             "description": "The name of the language model to use for the agent's reflection."
         },
     )
 
     answer_model: str = Field(
-        default="qwen2.5-coder-7b-instruct",
+        default="gpt-4.1",
         metadata={
             "description": "The name of the language model to use for the agent's answer."
         },
